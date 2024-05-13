e10b12fa3ab933f3f6d72ed65b58f4b5b030e9ea
piku-afk <sigmoidclient33@gmail.com>
2024-05-13 20:52:12 +0530

    refactor: rename `dashboard` to `client`

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const root = resolve('apps', 'dashboard');
-const outDir = resolve('build', 'dashboard');
+const root = resolve('apps', 'client');
+const outDir = resolve('build', 'client');
 
 export default defineConfig({
   root,
