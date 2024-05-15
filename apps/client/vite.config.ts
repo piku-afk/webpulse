cf2a469c1f4bdbeda69b12d1be079c67824a80e7
piku-afk <sigmoidclient33@gmail.com>
2024-05-15 19:08:43 +0530

    feat: add `@webpulse/client` module

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
